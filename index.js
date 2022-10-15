68a10eeab4fd9638b8aba3cb1415df179a965ebf
NicksyRop <nicksonkipkorir25@gmail.com>
2022-10-15 18:39:49 +0300

    added body-parser mongoose and created productSchema and its model

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const express = require("express");
-
 const app = express();
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+//middleware
+app.use(bodyParser.json());
+app.use(morgan("tiny"));
+
 require("dotenv").config();
-
 const api = process.env.API_URL;
+const mongo = process.env.MONGO_url;
 
-app.get("/", (req, res) => {
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: Number,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.get(api, (req, res) => {
   res.send("Hello Api");
 });
+
+app.post(`${api}/products`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+});
+
+main()
+  .then((res) => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect(mongo);
+}
 
 app.listen(3000, () => {
   console.log(`serevr running on http://localhost:3000`);
