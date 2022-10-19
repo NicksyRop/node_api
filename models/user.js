d9d8fa7722100be877efded8d4621e2c6fafc0c5
NicksyRop <nicksonkipkorir25@gmail.com>
2022-10-19 10:02:07 +0300

    added bcrypt to encrypt user password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,51 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({});
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+  street: {
+    type: String,
+    defailt: "",
+  },
+  apartment: {
+    type: String,
+    defailt: "",
+  },
+  zip: {
+    type: String,
+    defailt: "",
+  },
+  city: {
+    type: String,
+    defailt: "",
+  },
+  country: {
+    type: String,
+    defailt: "",
+  },
+});
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+});
 
 module.exports = mongoose.model("User", userSchema);
--- routers/users.js
+++ routers/users.js
@@ -3,5 +3,39 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User");
+var bcrypt = require("bcryptjs");
+
+router.get("/", async (req, res) => {
+  const users = await User.find();
+
+  if (!users) {
+    return res.status(500).json({ success: false });
+  } else {
+    return res.status(200).json(users);
+  }
+});
+
+router.post("/", async (req, res) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  newUser = await newUser.save();
+
+  if (!newUser) {
+    return res.status(400).json("User cannot be created");
+  }
+
+  return res.status(200).json(newUser);
+});
 
 module.exports = router;
